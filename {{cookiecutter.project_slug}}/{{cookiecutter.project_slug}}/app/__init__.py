df3d987928bfac5703a4a426f78dd79c341ab99c
levi malott <levi.malott@gmail.com>
2019-08-31 05:46:16 -0500

    Adding basic flask files and renamed app_name to project_slug

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+from flask import Flask
+
+from .api import blueprint as api_blueprint
+
+
+def create_app(config=Config):
+    app = Flask(__name__)
+
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    return app
